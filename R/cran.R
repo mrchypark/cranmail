6af09278b2a251becf26df5a2918743865c53742
chanyub Park <mrchypark@gmail.com>
2021-01-28 17:11:48 +0900

    add

--- R/cran.R
+++ R/cran.R
@@ -1,6 +1,7 @@
 library(httr)
 library(rvest)
 library(stringr)
+library(dplyr)
 
 tar <- "https://cran.r-project.org/web/packages/available_packages_by_name.html"
 
@@ -12,4 +13,15 @@ tar %>%
   str_replace_all(fixed("../.."), fixed("https://cran.r-project.org")) ->
   plist
 
+plist[1] %>%
+  read_html() %>%
+  html_table() %>%
+  .[[1]]  -> tobj
 
+tobj %>%
+  filter(str_detect(X1, "Author")) %>%
+  pull(X2)  -> author
+
+tobj %>%
+  filter(str_detect(X1, "Maintainer")) %>%
+  pull(X2)  -> mtner
---
+++ R/get_address.R
@@ -0,0 +1,3 @@
+get_address <- function(test){
+
+}
